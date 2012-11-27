7114d073cbb7cf167a21a65307045ba40bd22141
Simon Johansson <Simon.Johansson@elev.it-gymnasiet>
2012-11-27 10:23:30 +0100

    Added stuff that will help game

--- game.rb
+++ game.rb
@@ -4,10 +4,26 @@ class Game < Chingu::Window
 
 	#constructor
 	def initialize
-		super
+		super 800, 640
 		self.input = {esc: :exit}
 		@player = Player.create
+		@targets = []
+		5.times {@targets << Target.create}
+		@background_image = Gosu::Image.new(self, "world.png", true)
 	end
+
+
+	def update
+		super
+		Laser.each_bounding_circle_collision(Target) do |laser, target|
+			laser.destroy
+			target.destroy
+ 		end
+ 		Laserbomb.each_bounding_circle_collision(Target) do |laserbomb, target|
+			laserbomb.destroy
+			target.destroy
+	 	end
+ 	end
 end
 
 class Player < Chingu::GameObject
@@ -46,39 +62,56 @@ class Player < Chingu::GameObject
 	end
 	
 	def down
-		unless @y + 24 >= 600
+		unless @y + 24 >= 640
 			@y += 5
 		end
 	end
 
 	def fire
- 		Laser.create(:x => self.x, :y => self.y)
+ 		Laser.create(x: self.x, y: self.y)
   	end
 
   	def laserbomb
-  		Laserbomb.create(:x => self.x, :y => self.y)
+  		Laserbomb.create(x: self.x, y: self.y)
   	end
 
 end
 
 class Laser < Chingu::GameObject
-	has_traits :velocity
+	has_traits :velocity, :collision_detection, :bounding_circle, :timer
 
 	def setup
 		@image = Gosu::Image["redlaser.png"]
 		self.velocity_y = -10
+		Gosu::Sound["LASER1.wav"].play
+		after(500) do 
+			self.destroy
+		end
 	end
 
 end
 
 class Laserbomb < Chingu::GameObject
-	has_traits :velocity
+	has_traits :velocity, :collision_detection, :bounding_circle, :timer
 
 	def setup
 		@image = Gosu::Image["laserbomben.png"]
 		self.velocity_y = -10
+		after(500) do 
+			self.destroy
+		end
 	end
 
+end
+
+class Target < Chingu::GameObject
+	has_traits :collision_detection, :bounding_circle
+
+	def setup
+		@x = rand(800)
+		@y = 100
+		@image = Gosu::Image["target.png"]
+	end
 end
 
 Game.new.show	
